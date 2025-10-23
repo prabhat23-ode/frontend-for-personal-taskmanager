77fe9727c9226ed8fc3799ab95f43452364358a4
Prabhat Lohara <slohara30@gmail.com>
2025-10-23 10:17:11 +0530

    make login page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router'
 import './index.css'
 import App from './App.jsx'
-import { setAuthToken } from './Constant.js'
-
-// on startup, if token exists in localStorage, set default header
-const token = localStorage.getItem('taskAppToken');
-if (token) setAuthToken(token);
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
